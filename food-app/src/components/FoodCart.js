459b1bb944a3f797ce472263fd2417e06bca9a50
Hain Thit <hainlthit@gmail.com>
2022-01-25 12:32:58 -0500

    filter n search

--- food-app/src/components/FoodCart.js
+++ food-app/src/components/FoodCart.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function FoodCart({ item, onAdd }) {
+function FoodCart({ food, onAdd }) {
     return (
         <div>
-            <h1>{item.name}</h1>
-            <img src={item.image} height="200px" width="200px" alt={item.name}/>
-            <p>${item.price}</p>
-            <button onClick={() => onAdd(item)} >Add to Cart</button>
+            <h1>{food.name}</h1>
+            <img src={food.image} height="200px" width="200px" alt={food.name}/>
+            <p>${food.price}</p>
+            <button onClick={() => onAdd(food)} >Add to Cart</button>
         </div>
     );
   }
--- food-app/src/components/FoodContainer.js
+++ food-app/src/components/FoodContainer.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FoodCart from './FoodCart'
+import Filter from './Filter'
 
 function FoodContainer( { food, onAdd } ) {
+
+  const [categoryFilter, setCategoryFilter] = useState("All");
+  const [foodSearch, setFoodSearch] = useState("");
+
+  const foodItems = [...food]
+    .filter((food) => {
+      return categoryFilter === "All" || food.category === categoryFilter;
+    })
+    .filter((food) => {
+      return food.name.toLowerCase().includes(foodSearch.toLowerCase());
+    })
+    .map((food) => {
+      return <FoodCart key={food.id} food={food} onAdd={onAdd} />;
+    });
     return (
         <main className="block col-2">
           <h2>Yummy Japanese Food</h2>
-          { food.map(item => <FoodCart key={item.id} item={item} onAdd={onAdd} /> )}
+          {/* { food.map(item => <FoodCart key={item.id} item={item} onAdd={onAdd} /> )} */}
+          <Filter 
+          setCategoryFilter={setCategoryFilter}
+          setFoodSearch={setFoodSearch}
+          />
+
+          {foodItems}
           <div className="row">
           
           </div>
